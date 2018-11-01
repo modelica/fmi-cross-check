133f8eab33804b525b7a7664fc9faf7ed5a3fdf9
maplesim <lstewart@maplesoft.com>
2018-11-01 18:16:47 -0400

    Submit MapleSim 2018 FMUs 1.0,2.0 CS,ME

---
+++ fmus/2.0/cs/c-code/MapleSim/2018/Rectifier/Rectifier_cc.bat
@@ -0,0 +1,1 @@
+fmuCheck.win32.exe -l 3 -e Rectifier_cc.log -o Rectifier_cc.csv -c , -s 0.1 -h 1e-7 Rectifier.fmu
---
+++ fmus/2.0/cs/darwin64/MapleSim/2018/CoupledClutches/CoupledClutches_cc.sh
@@ -0,0 +1,1 @@
+./fmuCheck.darwin64  -i CoupledClutches_in.csv -e CoupledClutches_cc.log -o CoupledClutches_cc.csv -c , -s 1.5 -h 1e-2 CoupledClutches.fmu
