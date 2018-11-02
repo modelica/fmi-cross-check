5a2bc9eb4a9bd20973ed82f5b30afd74a1348e19
maplesim <lstewart@maplesoft.com>
2018-11-02 14:12:08 -0400

    Submit MapleSim 2018 FMUs 1.0CS, 2.0CS/ME

---
+++ fmus/2.0/cs/linux64/MapleSim/2018/CoupledClutches/CoupledClutches_cc.sh
@@ -0,0 +1,1 @@
+./fmuCheck.linux64 -e CoupledClutches_cc.log -i CoupledClutches_in.csv -o CoupledClutches_cc.csv -c , -s 1.5 -h 1e-2 CoupledClutches.fmu
---
+++ fmus/2.0/me/darwin64/MapleSim/2018/ControlledTemperature/ControlledTemperature_cc.sh
@@ -0,0 +1,1 @@
+ ./fmuCheck.darwin64 -e ControlledTemperature_cc.log -o ControlledTemperature_cc.csv -c , -s 10 -h 1e-3 ControlledTemperature.fmu
